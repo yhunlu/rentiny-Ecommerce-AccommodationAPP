e7bdc450443630484b46383b628afc32586a834a
yhunlu <yhunlu87@gmail.com>
2022-03-23 07:08:19 +0300

    implemented ant-design such as list, button, spin, avatar, alert, divider, skeleton.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Listings } from './sections';
 
 const App = () => {
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
