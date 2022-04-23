711c19620a09063c3ad8effaede1db49b0163966
yhunlu <yhunlu87@gmail.com>
2022-04-23 23:47:46 +0300

    added listingResolvers.

--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -1,5 +1,10 @@
 import merge from 'lodash.merge';
+import { listingResolvers } from './Listing';
 import { userResolvers } from './User';
 import { viewerResolvers } from './Viewer';
 
-export const resolvers = merge(userResolvers, viewerResolvers);
+export const resolvers = merge(
+  listingResolvers,
+  userResolvers,
+  viewerResolvers
+);
