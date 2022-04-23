ba49b59a8f0508ddf65c1eed2463dc580e858cd6
yhunlu <yhunlu87@gmail.com>
2022-04-23 23:57:33 +0300

    added bookingResolvers.

---
+++ backend/src/graphql/resolvers/Booking/index.ts
@@ -0,0 +1,13 @@
+import { IResolvers } from '@graphql-tools/utils';
+import { Database, Booking, Listing } from './../../../lib/types';
+
+export const bookingResolvers: IResolvers = {
+  Booking: {
+    id: (booking: Booking): string => {
+      return booking._id.toString();
+    },
+    listing: (booking: Booking, _args: unknown, { db }: { db: Database }): Promise<Listing | null> => {
+      return db.listings.findOne({ _id: booking.listing });
+    },
+  },
+};
--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -1,9 +1,11 @@
 import merge from 'lodash.merge';
+import { bookingResolvers } from './Booking';
 import { listingResolvers } from './Listing';
 import { userResolvers } from './User';
 import { viewerResolvers } from './Viewer';
 
 export const resolvers = merge(
+  bookingResolvers,
   listingResolvers,
   userResolvers,
   viewerResolvers
