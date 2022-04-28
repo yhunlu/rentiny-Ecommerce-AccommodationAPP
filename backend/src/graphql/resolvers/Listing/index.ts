b329592173df8dabc49379848bbc0eb64f8dc24a
yhunlu <yhunlu87@gmail.com>
2022-04-28 04:13:50 +0300

    refactoring entire listingResolvers and added listingsQuery as well as tested in Apollo GraphQL.

--- backend/src/graphql/resolvers/Listing/index.ts
+++ backend/src/graphql/resolvers/Listing/index.ts
@@ -1,82 +1,131 @@
 import { IResolvers } from '@graphql-tools/utils';
 import { Listing, Database, User } from './../../../lib/types';
-import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from './types';
 import { ObjectId } from 'mongodb';
 import { authorize } from '../../../lib/utils';
 import { Request } from 'express';
 import { calculateSkip } from './../utils';
+import { ListingArgs, ListingBookingsArgs, ListingBookingsData, ListingsArgs, ListingsData, ListingsFilter } from './types';
 
 export const listingResolvers: IResolvers = {
   Query: {
-    listing: async (
-      _root: undefined,
-      { id }: ListingArgs,
-      { db, req }: { db: Database; req: Request }
-    ): Promise<Listing> => {
-      try {
-        const listing = await db.listings.findOne({ _id: new ObjectId(id) });
-        if (!listing) {
-          throw new Error('Listing not found');
-        }
-
-        const viewer = await authorize(db, req);
-        if (viewer && viewer._id === listing.host) {
-          listing.authorized = true;
-        }
-
-        return listing;
-      } catch (error) {
-        throw new Error(`Failed to query listing: ${error}`);
-      }
-    },
+      listing: listingQuery,
+      listings: listingsQuery,
   },
   Listing: {
-    id: (listing: Listing): string => {
-      return listing._id.toString();
-    },
-    host: async (
-      listing: Listing,
-      _args: unknown,
-      { db }: { db: Database }
-    ): Promise<User> => {
-      const host = await db.users.findOne({ _id: listing.host });
-      if (!host) {
-        throw new Error('Host not found');
-      }
-      return host;
-    },
-    bookingsIndex: (listing: Listing): string => {
-      return JSON.stringify(listing.bookingsIndex);
-    },
-    bookings: async (
-      listing: Listing,
-      { limit, page }: ListingBookingsArgs,
-      { db }: { db: Database }
-    ): Promise<ListingBookingsData | null> => {
-      try {
-        if (!listing.authorized) {
-          return null;
-        }
-
-        const data: ListingBookingsData = {
-          total: 0,
-          result: [],
-        };
-
-        let cursor = db.bookings.find({
-          _id: { $in: listing.bookings },
-        });
-
-        cursor = cursor.skip(calculateSkip(page, limit));
-        cursor = cursor.limit(limit);
-
-        data.total = await cursor.count();
-        data.result = await cursor.toArray();
-
-        return data;
-      } catch (error) {
-        throw new Error(`Failed to query listing bookings: ${error}`);
-      }
-    },
+      id: getListingId,
+      host: getListingHost,
+      bookingsIndex: getListingBookingsIndex,
+      bookings: getListingBookings,
   },
 };
+
+async function listingQuery(
+  _root: undefined,
+  { id }: ListingArgs,
+  { db, req }: { db: Database; req: Request }
+): Promise<Listing | null> {
+  try {
+      const listing = await db.listings.findOne({
+          _id: new ObjectId(id),
+      });
+
+      if (!listing) {
+          throw new Error("Listing can't be found");
+      }
+
+      const viewer = await authorize(db, req);
+      if (viewer && viewer._id === listing.host) {
+          listing.authorized = true;
+      }
+
+      return listing;
+  } catch (error) {
+      throw new Error(`Failed to get listing: ${error}`);
+  }
+}
+
+async function listingsQuery(
+  _root: undefined,
+  { filter, limit, page }: ListingsArgs,
+  { db }: { db: Database }
+): Promise<ListingsData> {
+  try {
+      let cursor = await db.listings.find({});
+      if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          cursor = cursor.sort({
+              price: 1,
+          });
+      }
+      if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          cursor = cursor.sort({
+              price: -1,
+          });
+      }
+
+      cursor = cursor.limit(limit).skip(calculateSkip(page, limit));
+      const listings = await cursor.toArray();
+      const count = await cursor.count();
+
+      return {
+          total: count,
+          result: listings,
+      };
+  } catch (error) {
+      throw new Error(`Failed to query listings: ${error}`);
+  }
+}
+
+function getListingId(listing: Listing): string {
+  return listing._id.toString();
+}
+
+async function getListingHost(
+  listing: Listing,
+  _args: undefined,
+  { db }: { db: Database }
+): Promise<User> {
+  const host = await db.users.findOne({
+      _id: listing.host,
+  });
+
+  if (!host) {
+      throw new Error("Host can't be found");
+  }
+
+  return host;
+}
+
+function getListingBookingsIndex(listing: Listing): string {
+  return JSON.stringify(listing.bookingsIndex);
+}
+
+async function getListingBookings(
+  listing: Listing,
+  { limit, page }: ListingBookingsArgs,
+  { db }: { db: Database }
+): Promise<ListingBookingsData | null> {
+  try {
+      if (!listing.authorized) {
+          return null;
+      }
+
+      const data: ListingBookingsData = {
+          total: 0,
+          result: [],
+      };
+
+      let cursor = await db.bookings.find({
+          _id: { $in: listing.bookings },
+      });
+
+      cursor = cursor.skip(calculateSkip(page, limit));
+      cursor = cursor.limit(limit);
+
+      data.total = await cursor.count();
+      data.result = await cursor.toArray();
+
+      return data;
+  } catch (error) {
+      throw new Error(`Failed to query listing bookings: ${error}`);
+  }
+}
--- frontend/src/sections/Listing/index.tsx
+++ frontend/src/sections/Listing/index.tsx
@@ -53,64 +53,65 @@ const Listing = () => {
   }
 
   const listing = data ? data.listing : null;
-  // const listingBookings = listing ? listing.bookings : null;
-  const listingBookings = {
-    total: 4,
-    result: [
-      {
-        id: '5daa530eefc64b001767247c',
-        tenant: {
-          id: '117422637055829818290',
-          name: 'User X',
-          avatar:
-            'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
-          __typename: 'User',
-        },
-        checkIn: '2019-10-29',
-        checkOut: '2019-10-31',
-        __typename: 'Booking',
-      },
-      {
-        id: '5daa530eefc64b001767247d',
-        tenant: {
-          id: '117422637055829818290',
-          name: 'User X',
-          avatar:
-            'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
-          __typename: 'User',
-        },
-        checkIn: '2019-11-01',
-        checkOut: '2019-11-03',
-        __typename: 'Booking',
-      },
-      {
-        id: '5daa530eefc64b001767247g',
-        tenant: {
-          id: '117422637055829818290',
-          name: 'User X',
-          avatar:
-            'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
-          __typename: 'User',
-        },
-        checkIn: '2019-11-05',
-        checkOut: '2019-11-09',
-        __typename: 'Booking',
-      },
-      {
-        id: '5daa530eefc64b001767247f',
-        tenant: {
-          id: '117422637055829818290',
-          name: 'User X',
-          avatar:
-            'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
-          __typename: 'User',
-        },
-        checkIn: '2019-11-10',
-        checkOut: '2019-11-11',
-        __typename: 'Booking',
-      },
-    ],
-  } as any;
+  const listingBookings = listing ? listing.bookings : null;
+  
+  // const listingBookings = {
+  //   total: 4,
+  //   result: [
+  //     {
+  //       id: '5daa530eefc64b001767247c',
+  //       tenant: {
+  //         id: '117422637055829818290',
+  //         name: 'User X',
+  //         avatar:
+  //           'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
+  //         __typename: 'User',
+  //       },
+  //       checkIn: '2019-10-29',
+  //       checkOut: '2019-10-31',
+  //       __typename: 'Booking',
+  //     },
+  //     {
+  //       id: '5daa530eefc64b001767247d',
+  //       tenant: {
+  //         id: '117422637055829818290',
+  //         name: 'User X',
+  //         avatar:
+  //           'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
+  //         __typename: 'User',
+  //       },
+  //       checkIn: '2019-11-01',
+  //       checkOut: '2019-11-03',
+  //       __typename: 'Booking',
+  //     },
+  //     {
+  //       id: '5daa530eefc64b001767247g',
+  //       tenant: {
+  //         id: '117422637055829818290',
+  //         name: 'User X',
+  //         avatar:
+  //           'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
+  //         __typename: 'User',
+  //       },
+  //       checkIn: '2019-11-05',
+  //       checkOut: '2019-11-09',
+  //       __typename: 'Booking',
+  //     },
+  //     {
+  //       id: '5daa530eefc64b001767247f',
+  //       tenant: {
+  //         id: '117422637055829818290',
+  //         name: 'User X',
+  //         avatar:
+  //           'https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100',
+  //         __typename: 'User',
+  //       },
+  //       checkIn: '2019-11-10',
+  //       checkOut: '2019-11-11',
+  //       __typename: 'Booking',
+  //     },
+  //   ],
+  // } as any;
 
   const listingDetailsElement = listing ? (
     <ListingDetails listing={listing} />
