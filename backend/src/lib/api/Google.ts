770416dda29134cb7bf08125712d1be5d79bc297
yhunlu <yhunlu87@gmail.com>
2022-05-02 03:58:35 +0300

    setup google map client with geocode functionality.

--- backend/src/lib/api/Google.ts
+++ backend/src/lib/api/Google.ts
@@ -1,10 +1,39 @@
 import { google } from 'googleapis';
+import { Client, AddressComponent } from '@googlemaps/google-maps-services-js';
+import axios from 'axios';
 
 const oAuth2Client = new google.auth.OAuth2(
   `${process.env.GOOGLE_CLIENT_ID}`,
   `${process.env.GOOGLE_CLIENT_SECRET}`,
   `${process.env.GOOGLE_REDIRECT_URL}`
 );
+
+const maps = new Client({});
+
+const parseAddress = (addressComponents: AddressComponent[]) => {
+  let country = null;
+  let admin = null;
+  let city = null;
+
+  for (const component of addressComponents) {
+    if (component.types[0] === 'country') {
+      country = component.long_name;
+    }
+
+    if (component.types[0] === 'administrative_area_level_1') {
+      admin = component.long_name;
+    }
+
+    if (
+      component.types[0] === 'locality' ||
+      component.types[0] === 'postal_town'
+    ) {
+      city = component.long_name;
+    }
+  }
+
+  return { country, admin, city };
+};
 
 export const Google = {
   authUrl: oAuth2Client.generateAuthUrl({
@@ -28,5 +57,20 @@ export const Google = {
     return {
       user: data,
     };
+  },
+  geocode: async (address: string) => {
+    const res = await maps.geocode({
+      params: {
+        address,
+        key: `${process.env.GOOGLE_MAPS_API_KEY}`,
+      },
+      timeout: 1000, // milliseconds
+    });
+
+    if (res.status < 200 || res.status > 299) {
+      throw new Error(res.data.error_message);
+    }
+
+    return parseAddress(res.data.results[0].address_components);
   },
 };
