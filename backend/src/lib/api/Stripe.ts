dbc1ebd3ad88a0a278fce2f870ca203f05efca60
yhunlu <yhunlu87@gmail.com>
2022-05-14 01:12:34 +0300

    added stripe-api.

---
+++ backend/src/lib/api/Stripe.ts
@@ -0,0 +1,24 @@
+import stripe from 'stripe';
+
+// grepper typescript initialize stripe connection
+const client = new stripe(`${process.env.STRIPE_SECRET_KEY}`, {
+  apiVersion: '2020-08-27',
+});
+// end grepper
+
+// grepper typescript connect stripe with OAuth
+export const Stripe = {
+  connect: async (code: string) => {
+    const response = await client.oauth.token({
+      grant_type: 'authorization_code',
+      code,
+    });
+
+    if (!response) {
+      return new Error('Stripe connect error');
+    }
+
+    return response;
+  },
+};
+// end grepper
