d6c41d51c0302ade6507f2a1241ea3475135ae2b
yhunlu <yhunlu87@gmail.com>
2022-05-13 04:42:14 +0300

    added stripe mutation resolvers boilerplate.

--- backend/src/graphql/resolvers/Viewer/index.ts
+++ backend/src/graphql/resolvers/Viewer/index.ts
@@ -162,6 +162,12 @@ export const viewerResolvers: IResolvers = {
         throw new Error(`Failed to log user out: ${error}`);
       }
     },
+    connectStripe: (): Viewer => {
+      return { didRequest: true };
+    },
+    disconnectStripe: (): Viewer => {
+      return { didRequest: true };
+    }
   },
   Viewer: {
     id: (viewer: Viewer): string | undefined => {
