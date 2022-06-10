8831c921678a058412b245be95cc7004180bb855
yhunlu <yhunlu87@gmail.com>
2022-06-11 00:53:43 +0300

    included Host component.

--- frontend/src/sections/Host/index.tsx
+++ frontend/src/sections/Host/index.tsx
@@ -1,10 +1,23 @@
+import { Layout, Typography } from 'antd'
 import React from 'react'
+
+const { Content } = Layout;
+const { Title, Text } = Typography;
 
 type Props = {}
 
 const Host = (props: Props) => {
   return (
-    <div>Host</div>
+    <Content className="host-content">
+      <div className="host__form-header">
+        <Title level={3} className="host__form-title">
+          Hi! Let's get started listing your place.
+        </Title>
+        <Text type="secondary">
+          In this form, we'll collect some basic and additionl information about your listing.
+        </Text>
+      </div>
+    </Content>
   )
 }
 
