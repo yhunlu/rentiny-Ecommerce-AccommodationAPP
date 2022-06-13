bd7d2264389f9381a691885cc6d26619655bb325
yhunlu <yhunlu87@gmail.com>
2022-06-13 04:47:38 +0300

    used form-component from ant-design to build host section.

--- frontend/src/sections/Host/index.tsx
+++ frontend/src/sections/Host/index.tsx
@@ -1,45 +1,177 @@
-import { Layout, Typography } from 'antd'
-import React from 'react'
+import {
+  Form,
+  Input,
+  InputNumber,
+  Layout,
+  Radio,
+  Typography,
+  Upload,
+} from 'antd';
+import { UploadChangeParam } from 'antd/lib/upload';
 import { Viewer } from '../../lib/types';
 import { Link } from 'react-router-dom';
+import { ListingType } from '../../lib/graphql/globalTypes';
+import { BankTwoTone, CloudUploadOutlined, HomeTwoTone, LoadingOutlined } from '@ant-design/icons';
+import { displayErrorMessage } from '../../lib/utils';
+import { useState } from 'react';
 
 const { Content } = Layout;
 const { Title, Text } = Typography;
+const { Item } = Form;
 
 interface Props {
   viewer: Viewer;
 }
 
+const beforeImageUpload = (file: File) => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  if (!isJpgOrPng) {
+    displayErrorMessage("You're only able to upload valid JPG or PNG files!");
+    return false;
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    displayErrorMessage(
+      "You're only able to upload valid image files of under 2MB in size!"
+    );
+    return false;
+  }
+  return isJpgOrPng && isLt2M;
+};
+
+const getBase64Value = (
+  img: File | Blob,
+  callback: (imageBase64Value: string) => void
+) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(img);
+  reader.onload = () => {
+    callback(reader.result as string);
+  };
+};
+
 const Host = ({ viewer }: Props) => {
+  const [imageLoading, setImageLoading] = useState(false);
+  const [imageBase64Value, setImageBase64Value] = useState<string | null>(null);
+
+  const handleImageUpload = (info: UploadChangeParam) => {
+    const { file } = info;
+
+    if (file.status === 'uploading') {
+      setImageLoading(true);
+      return;
+    }
+
+    if (file.status === 'done' && file.originFileObj) {
+      getBase64Value(file.originFileObj, imageBase64Value => {
+        setImageBase64Value(imageBase64Value);
+        setImageLoading(false);
+      });
+    }
+  };
 
   if (!viewer.id || !viewer.hasWallet) {
     return (
       <Content className="host-content">
-      <div className="host__form-header">
-        <Title level={4} className="host__form-title">
-          You'll have to be signed in and connected with Stripe to host a listing!
-        </Title>
-        <Text type="secondary">
-          We only allow users who've signed in to our application and have connected with Stripe to host new listings.
-          You can sign in at the <Link to="/login">LOGIN</Link> page and connect with Stripe shortly after.
-        </Text>
-      </div>
-    </Content>
-    )
+        <div className="host__form-header">
+          <Title level={4} className="host__form-title">
+            You'll have to be signed in and connected with Stripe to host a
+            listing!
+          </Title>
+          <Text type="secondary">
+            We only allow users who've signed in to our application and have
+            connected with Stripe to host new listings. You can sign in at the{' '}
+            <Link to="/login">LOGIN</Link> page and connect with Stripe shortly
+            after.
+          </Text>
+        </div>
+      </Content>
+    );
   }
 
   return (
     <Content className="host-content">
-      <div className="host__form-header">
-        <Title level={3} className="host__form-title">
-          Hi! Let's get started listing your place.
-        </Title>
-        <Text type="secondary">
-          In this form, we'll collect some basic and additionl information about your listing.
-        </Text>
-      </div>
-    </Content>
-  )
-}
+      <Form layout="vertical">
+        <div className="host__form-header">
+          <Title level={3} className="host__form-title">
+            Hi! Let's get started listing your place.
+          </Title>
+          <Text type="secondary">
+            In this form, we'll collect some basic and additional information
+            about your listing.
+          </Text>
+        </div>
 
-export default Host
+        <Item label="Accommodation Type">
+          <Radio.Group>
+            <Radio.Button value={ListingType.APARTMENT}>
+              <BankTwoTone /> <span>Apartment</span>
+            </Radio.Button>
+            <Radio.Button value={ListingType.HOUSE}>
+              <HomeTwoTone /> <span>House</span>
+            </Radio.Button>
+          </Radio.Group>
+        </Item>
+
+        <Item label="Title" extra="Max character count of 45">
+          <Input
+            maxLength={45}
+            placeholder="The iconic and luxurious Bel-Air mansion"
+          />
+        </Item>
+
+        <Item label="Description of listing" extra="Max character count of 400">
+          <Input.TextArea
+            rows={3}
+            maxLength={400}
+            placeholder="Modern, clean, and iconic home of the Fresh Price. Situated in the heart of Bel-Air, Los Angeles."
+          />
+        </Item>
+
+        <Item label="City/Town">
+          <Input placeholder="Los Angeles" />
+        </Item>
+
+        <Item label="State/Province">
+          <Input placeholder="California" />
+        </Item>
+
+        <Item label="Zip/Postal Code">
+          <Input placeholder="Please enter a zip code for your listing!" />
+        </Item>
+
+        <Item
+          label="Image"
+          extra="Images have to be under 2MB in size and of type JPG or PNG"
+        >
+          <div className="host__form-image-upload">
+            <Upload
+              name="image"
+              listType="picture-card"
+              showUploadList={false}
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              beforeUpload={beforeImageUpload}
+              onChange={handleImageUpload}
+            >
+              {imageBase64Value ? (
+                <img src={imageBase64Value} alt="Listing" />  
+                ) : (
+                  <div>
+                    {imageLoading ? <LoadingOutlined /> : <CloudUploadOutlined />}
+                    <div className="ant-upload-text">Upload</div>
+                  </div>
+                )
+              }
+            </Upload>
+          </div>
+        </Item>
+
+        <Item label="Price" extra="All prices in $USD/day">
+          <InputNumber min={0} placeholder="120" />
+        </Item>
+      </Form>
+    </Content>
+  );
+};
+
+export default Host;
