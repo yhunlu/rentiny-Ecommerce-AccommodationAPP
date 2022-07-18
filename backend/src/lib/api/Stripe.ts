0521321f2ebabf62b135abe5ea4a8265b8d20735
yhunlu <yhunlu87@gmail.com>
2022-07-18 03:39:24 +0300

    added creatingDirectCharges in Stripe api.

--- backend/src/lib/api/Stripe.ts
+++ backend/src/lib/api/Stripe.ts
@@ -1,12 +1,12 @@
 import stripe from 'stripe';
 
-//grepper Typescript initialize stripe connection
+//grepper typescript initialize stripe connection
 const client = new stripe(`${process.env.STRIPE_SECRET_KEY}`, {
   apiVersion: '2020-08-27',
 });
 //end grepper
 
-//grepper Typescript connect stripe with OAuth
+//grepper typescript connect stripe with OAuth and direct apply charge
 export const Stripe = {
   connect: async (code: string) => {
     const response = await client.oauth.token({
@@ -15,6 +15,23 @@ export const Stripe = {
     });
 
     return response;
+  },
+  charge: async (amount: number, source: string, stripeAccount: string) => {
+    const res = await client.charges.create(
+      {
+        amount,
+        currency: 'usd',
+        source,
+        application_fee_amount: Math.round(amount * 0.05),
+      },
+      {
+        stripe_account: stripeAccount,
+      }
+    );
+
+    if (res.status !== 'succeeded') {
+      throw new Error('failed to create charge with Stripe');
+    }
   },
 };
 //end grepper
